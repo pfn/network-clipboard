1d81d769df19f990d86997eb373b5b0947387ca8
Perry Nguyen <pfnguyen@hanhuy.com>
2014-01-24 20:12:47 -0800

    initial version

---
+++ NetworkClipboard/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetworkClipboard
+{
+    public partial class Form1 : Form
+    {
+        public const int WM_CLIPBOARDUPDATE = 0x031D;
+        private bool allowShow = false;
+        private NotifyIcon icon;
+        public Form1()
+        {
+            InitializeComponent();
+            icon = new NotifyIcon();
+            icon.Icon = Properties.Resources.ClipboardIcon;
+            icon.Visible = true;
+            icon.Text = "Network Clipboard";
+            icon.Click += delegate
+            {
+                allowShow = true;
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+            };
+            Icon = Properties.Resources.ClipboardIcon;
+            Hide();
+        }
+
+        protected override void SetVisibleCore(bool value)
+        {
+            base.SetVisibleCore(allowShow ? value : false);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            icon.Dispose();
+            Program.state.running = false;
+            Program.evt.Set();
+            base.OnClosed(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_CLIPBOARDUPDATE:
+                    clipboardData.Text = "";
+                    break;
+            }
+            base.WndProc(ref m);
+        }
+        public void SetText(String text)
+        {
+            clipboardData.Text = text;
+            clipboardData.Select(0, 0);
+            NotifyUpdate(text);
+        }
+        public void NotifyUpdate(String text)
+        {
+            icon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            icon.BalloonTipTitle = "Clipboard Updated";
+            icon.BalloonTipText = text;
+            icon.ShowBalloonTip(1000);
+        }
+
+
+    }
+}
---
+++ NetworkClipboard/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetworkClipboard
+{
+    class State
+    {
+        public volatile bool running = true;
+    }
+    static class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+
+        public static ManualResetEvent evt = new ManualResetEvent(false);
+        public static State state = new State();
+        private static Form1 form;
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            form = new Form1();
+            AddClipboardFormatListener(form.Handle);
+
+            var thread = new Thread(new ThreadStart(NetworkListener));
+            thread.Start();
+            Application.Run(form);
+        }
+
+        static void NetworkListener()
+        {
+            //form.BeginInvoke(new MethodInvoker(Show));
+            //Show();
+            var listener = new TcpListener(IPAddress.Parse("0.0.0.0"), 4573);
+            listener.Start();
+            while (state.running)
+            {
+                evt.Reset();
+                listener.BeginAcceptSocket(new AsyncCallback(ConnectSocket), listener);
+                evt.WaitOne();
+            }
+        }
+
+        delegate void ShowInvoker(String text);
+        static void ConnectSocket(IAsyncResult iar)
+        {
+            var listener = (TcpListener)iar.AsyncState;
+            var socket = listener.EndAcceptSocket(iar);
+
+            byte[] buf = new byte[32768];
+            int read = -1;
+            var builder = new StringBuilder();
+            while (read != 0)
+            {
+                read = socket.Receive(buf);
+                var data = Encoding.UTF8.GetChars(buf, 0, read);
+                builder.Append(data);
+            }
+            form.BeginInvoke(new ShowInvoker(Show), builder.ToString());
+            socket.Close();
+            socket.Dispose();
+            evt.Set();
+        }
+        static void Show(String text)
+        {
+            text = text.IndexOf("\r\n") == -1 ? text.Replace("\n", "\r\n") : text;
+            RemoveClipboardFormatListener(form.Handle);
+            Clipboard.SetText(text);
+            form.SetText(text);
+            AddClipboardFormatListener(form.Handle);
+        }
+    }
+}
